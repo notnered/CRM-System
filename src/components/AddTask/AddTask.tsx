0189cada8d4f7eac8169160c859a65183380763a
Konstantin Nered <konstantin.n616@gmail.com>
2025-08-28 20:18:52 +0700

    Создал компонент добавления задач

--- src/components/AddTask/AddTask.tsx
+++ src/components/AddTask/AddTask.tsx
@@ -1,6 +1,39 @@
+import { useRef } from 'react';
 import styles from './AddTask.module.scss';
 
-export default function AddTask() {
+export default function AddTask({ refreshFunc }: { refreshFunc: () => void }) {
+    const inputRef = useRef<HTMLInputElement>(null);
+    async function handleSubmit() {
+        if (!inputRef.current) {
+            console.error('Текст задачи пустой');
+            return;
+        }
+
+        if (inputRef.current.value.length < 2) {
+            console.error('Длина задачи меньше 2 символов');
+            return;
+        }
+
+        const taskTitle = inputRef.current.value;
+
+        const response = await fetch('https://easydev.club/api/v1/todos', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                title: taskTitle,
+                isDone: false,
+            }),
+        });
+        if (!response.ok) {
+            console.error('Ошибка при добавлении задачи');
+            return;
+        }
+
+        refreshFunc();
+    }
+
     return (
         <div className={styles.box}>
             <input
@@ -8,8 +41,15 @@ export default function AddTask() {
                 className={styles.input}
                 type='text'
                 placeholder='Текст задачи'
+                ref={inputRef}
+                required
+                aria-required
             />
-            <button type='submit' className={styles.button}>
+            <button
+                onClick={handleSubmit}
+                type='submit'
+                className={styles.button}
+            >
                 Добавить задачу
             </button>
         </div>
