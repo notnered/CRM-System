d00fb485c9e0b47f33991574ae1130c355f7c719
Konstantin Nered <konstantin.n616@gmail.com>
2025-08-28 21:40:34 +0700

    Завершил разработку верхнего уровня приложения

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import AddTask from './components/AddTask/AddTask';
+import FilterTask from './components/FilterTask/FilterTask';
+import ListTask, { type Task } from './components/ListTask/ListTask';
+import { useState, useEffect } from 'react';
+import styles from './App.module.scss';
+
+export type Response = {
+    data: Task[];
+    info: {
+        all: number;
+        completed: number;
+        inWork: number;
+    };
+    meta: {
+        totalAmount: number;
+    };
+};
+
+export type Filter = 'all' | 'inWork' | 'completed';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [todosData, setTodosData] = useState<Response | null>(null);
+    const [filter, setFilter] = useState<Filter>('all');
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    async function refreshData() {
+        const response = await fetch('https://easydev.club/api/v1/todos');
+        const json = await response.json();
+        console.log(json);
+        setTodosData(json);
+    }
+
+    async function filterCallback(filter: Filter) {
+        setFilter(filter);
+    }
+
+    useEffect(() => {
+        (async function () {
+            refreshData();
+        })();
+    }, []);
+
+    if (!todosData) return null;
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.appBox}>
+                <AddTask refreshFunc={refreshData} />
+                <FilterTask
+                    currentFilter={filter}
+                    changeFilter={filterCallback}
+                    todoInfo={todosData.info}
+                />
+                <ListTask
+                    currentFilter={filter}
+                    refreshFunc={refreshData}
+                    todoList={todosData.data}
+                />
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.scss'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
+import './global.scss';
+import App from './App.tsx';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+    },
+    {
+        path: '*',
+        element: <div>404</div>,
+    },
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
