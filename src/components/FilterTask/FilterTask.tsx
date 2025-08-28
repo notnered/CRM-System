fdf4963cd809c7e7a7d9abb02c92dd524b66862c
Konstantin Nered <konstantin.n616@gmail.com>
2025-08-28 20:19:46 +0700

    Добавил возможность переключать фильтр задач

--- src/components/FilterTask/FilterTask.tsx
+++ src/components/FilterTask/FilterTask.tsx
@@ -1,21 +1,59 @@
 import styles from './FilterTask.module.scss';
+import type { Response, Filter } from '../../App';
+import { useEffect, useState } from 'react';
 
 const options = [
     { filter: 'all', label: 'Все' },
-    { filter: 'active', label: 'Активные' },
     { filter: 'completed', label: 'Выполненные' },
+    { filter: 'inWork', label: 'В прогрессе' },
 ];
 
-export default function FilterTask() {
+type TodoInfo = Response['info'];
+
+type FilteredAmount = {
+    filter: string;
+    label: string;
+    amount: number;
+};
+
+export default function FilterTask({
+    currentFilter,
+    todoInfo,
+    changeFilter,
+}: {
+    currentFilter: Filter;
+    todoInfo: TodoInfo;
+    changeFilter: (filter: Filter) => void;
+}) {
+    const [filters, setFilters] = useState<FilteredAmount[]>([]);
+
+    useEffect(() => {
+        const unitedData = options.map((option, index) => {
+            return {
+                filter: option.filter,
+                label: option.label,
+                amount: Object.values(todoInfo)[index],
+            };
+        });
+        setFilters(unitedData);
+    }, []);
+
     return (
         <div className={styles.filterBox}>
-            {options.map((option, index) => {
+            {filters.map((filter, index) => {
                 return (
                     <span
-                        className={styles.filter}
-                        key={`${option.filter}-${index}`}
+                        className={`${styles.filter} ${
+                            currentFilter === filter.filter && styles.active
+                        }`}
+                        key={`${filter.filter}-${index}`}
+                        id={filter.filter}
+                        onClick={(e) =>
+                            changeFilter(String(e.currentTarget.id) as Filter)
+                        }
                     >
-                        {option.label} (0)
+                        {filter.label}{' '}
+                        {filter.amount > 0 && `(${filter.amount})`}
                     </span>
                 );
             })}
